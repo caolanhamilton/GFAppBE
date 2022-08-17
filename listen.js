d3dcae4c3f8ab1397417f1080e2abe3109a6fba5
caolanhamilton <caolanhamilton75@gmail.com>
2022-08-17 10:30:39 +0100

    Revert listen file

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require("./app.js");
-const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
-});
+const { PORT = 8080 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
